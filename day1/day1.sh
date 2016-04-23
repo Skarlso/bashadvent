059dd46c1763af286322c8d7d526437f02cd39bc
Gergely Brautigam <skarlso777@gmail.com>
2016-04-23 09:41:08 +0200

    First day.

--- day1/day1.sh
+++ day1/day1.sh
@@ -2,4 +2,13 @@
 
 value=$(<input.txt)
 
-echo $value
+counter=0
+for (( i=0; i<${#value}; i++ )); do
+    if [ "${value:$i:1}" == "(" ]; then
+        (( counter++ ))
+    else
+        (( counter-- ))
+    fi
+done
+
+echo $counter
