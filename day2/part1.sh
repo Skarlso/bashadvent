d71cc6a6c0a8bb70838f3593bd6fc35a06452c0c
Gergely Brautigam <skarlso777@gmail.com>
2016-04-24 22:34:10 +0200

    Part 1 done.

--- day2/part1.sh
+++ day2/part1.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-declare -a arr
+declare total
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
-    arr+=($line)
+    IFS='x' read -ra split <<< "$line"
+    l=${split[0]}
+    w=${split[1]}
+    h=${split[2]}
+
+    (( total+=(2*l*w)+(2*w*h)+(2*l*h) ))
+    # IFS -> Tell it to delimit with new line which sort requires.
+    # -n -> numbers
+    # <<< -> Input stream
+    IFS=$'\n' sorted=($(sort -n <<<"${split[*]}"))
+    (( total+=sorted[0]*sorted[1] ))
+    unset IFS
 done < "input.txt"
 
-echo "${arr[@]}"
+echo $total
